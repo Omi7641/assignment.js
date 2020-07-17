e8df82080aa8d55853c1a5896c5090b2537801c5
Omi7641 <67516388+Omi7641@users.noreply.github.com>
2020-07-17 21:23:04 +0600

    update tinyFriend code

--- tinyFriend.js
+++ tinyFriend.js
@@ -2,11 +2,11 @@
     var shortest = arr[0];
     for( var i=0; i < arr.length ; i++) {
         var element = arr[i];
-        if( element > shortest){
-            min = element;
+        if( element.length < shortest.length){
+           shortest= element;
         }
     }
   return shortest;
   }
-var shortesetName =tinyFriend(["mehedi", "hasanSFDGfsD", "omi", "chandrika"]);
+var shortesetName =tinyFriend(["mehedi", "hasan","omi", "chandrika"]);
 console.log(shortesetName);
